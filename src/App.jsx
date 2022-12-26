680fb2b8af19cf4b6055d1bb20b2398ba91b5f0f
HarveyNilton <65696942+HarveyNilton@users.noreply.github.com>
2022-12-26 12:51:12 -0500

    h

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <img src="/hoja.jpg" alt="" className="fondo" />
       <div className='container-info'>
-        <h2 className="teInvito">Te5 Invito a celebrar</h2>
+        <h2 className="teInvito">Te Invito a celebrar</h2>
         <h2 className="Micumple">Mi Cumpleaños</h2>
         <h3 className="fecha-hora">El dia Sabado 11 de Junio <br />a partir de las 08:00pm</h3>
         <h3 className="domicilo">En mi domicilio</h3>
