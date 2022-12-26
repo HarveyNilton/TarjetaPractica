d4e8b00b13fc2bc2dcc2b5be90cf543ab1981c62
HarveyNilton <65696942+HarveyNilton@users.noreply.github.com>
2022-12-26 13:32:56 -0500

    tr

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,16 @@ function App() {
   return (
     <div className="App">
       <img src="/hoja.jpg" alt="" className="fondo" />
+      <div className='edad-conta'>
+       <h1 className='edad'>5</h1>
+      </div>
       <div className='container-info'>
         <h2 className="teInvito">Te Invito a celebrar</h2>
         <h2 className="Micumple">Mi Cumpleaños</h2>
         <h3 className="fecha-hora">El dia Sabado 11 de Junio <br />a partir de las 08:00pm</h3>
         <h3 className="domicilo">En mi domicilio</h3>
         <h3 className="te-espero">Te espero</h3>
-        <h1 className="names">Harvey Nilton <br /> Lopez Quiñones</h1>
+        <h1 className="names">Maria del Pilar <br /> Salazar Ruiz</h1>
       </div>
     </div>
   )
