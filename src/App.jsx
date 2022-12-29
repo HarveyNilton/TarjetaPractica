b7725a99a4b35bbaa48844d248101eaaf91d90fc
HarveyNilton <65696942+HarveyNilton@users.noreply.github.com>
2022-12-28 21:33:53 -0500

    tr

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,7 @@ function App() {
         <h2 className="teInvito">Te Invito a celebrar</h2>
         <h2 className="Micumple">Mi Cumpleaños</h2>
         <h3 className="fecha-hora">El dia Sabado 11 de Junio <br />a partir de las 08:00pm</h3>
+       
         <h3 onClick={mostarMapa} className="domicilo">En mi domicilio</h3>
         <h3 className="te-espero">Te espero</h3>
         <h1 className="names">Maria del Pilar <br /> Salazar Ruiz</h1>
