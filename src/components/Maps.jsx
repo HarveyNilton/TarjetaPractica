32f3c367be18f199c2648931191e0234eec99d9a
HarveyNilton <65696942+HarveyNilton@users.noreply.github.com>
2022-12-31 10:30:03 -0500

    co

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -13,8 +13,7 @@ import {
 const Maps = () => {
 
     const svgMarker = {
-        path:
-          "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+        path:"M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
         fillColor: "blue",
         fillOpacity: 0.6,
         strokeWeight: 0,
@@ -37,18 +36,18 @@ const Maps = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-       { /* <CircleMarker
-          center={[-3.7511446, -73.2640492]}
+       {  <CircleMarker
+          center={[-3.761259, -73.268645]}
           color={"#000000"}
           fillColor={"#FDD876"}
           stroke={true}
           fillOpacity={true}
           weight={1}
           icon={svgMarker}
-    />*/}
+    />}
         
-        <Marker position={[-3.761259, -73.268645]}/> 
-        
+       {/*<Marker position={[-3.761259, -73.268645]}/>*/ }
+
       </MapContainer>
       
        </div>
